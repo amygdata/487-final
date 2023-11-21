ed8d62b9036029b493304ce74ab77b339f404cde
amygdata <39467124+amygdata@users.noreply.github.com>
2023-11-20 21:14:37 -0500

    Refactored data loading

--- load_data.py
+++ load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import chardet 
 
-def detect_encoding(file_path: str):
+def detect_encoding(file_path: str) -> str:
     """
     Detects the encoding of a file
     """
@@ -10,19 +10,19 @@ def detect_encoding(file_path: str):
 
     return result['encoding']
 
-def load_sem_eval_data(txt_path: str, target: str):
+def preprocess_data(data: pd.DataFrame, target: str) -> pd.DataFrame:
     """
-    Loads the SemEval 2016 dataset and extracts rows that contain the target
+    Preprocesses the data
 
     Args:
-        txt_path: Path to the SemEval 2016 dataset (.txt format)
+        data: The dataframe to preprocess
         target: The target to extract from the dataset
 
     Returns:
-        Pandas dataframe containing the dataset
+        The preprocessed dataframe
     """
-    # Load dataset
-    data = pd.read_csv(txt_path, sep='\t', encoding=detect_encoding(txt_path))
+    # Deep copy the dataframe
+    data = data.copy()
 
     # Remove targets we do not care about
     data = data.loc[data['Target'] == target]
@@ -34,3 +34,28 @@ def load_sem_eval_data(txt_path: str, target: str):
     data['Tweet'] = data['Tweet'].str.lower()
 
     return data
+
+def load_sem_eval_data(target: str) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Loads the SemEval 2016 dataset and extracts rows that contain the target
+
+    Args:
+        target: The target (e.g., "Climate Change is a Real Concern") to extract from the dataset
+
+    Returns:
+        Two pandas dataframes containing the training and test datasets
+    """
+
+    # Load training dataset
+    train_data_path = 'semeval2016-task6-trainingdata.txt'
+    train_data = pd.read_csv(train_data_path, sep='\t', encoding=detect_encoding(train_data_path))
+
+    # Load test dataset
+    test_data_path = 'SemEval2016-Task6-subtaskA-testdata-gold.txt'
+    test_data = pd.read_csv(test_data_path, sep='\t', encoding=detect_encoding(test_data_path))
+
+    # Preprocess training and test data
+    train_data = preprocess_data(train_data, target)
+    test_data = preprocess_data(test_data, target)
+
+    return train_data, test_data
--- naive_bayes.py
+++ naive_bayes.py
@@ -2,11 +2,9 @@ from load_data import load_sem_eval_data
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import f1_score
 
-def print_stance_statistics(data: pd.DataFrame):
+def print_stance_statistics(data: pd.DataFrame) -> None:
     """
     Prints the number of tweets in each stance
     """
@@ -16,15 +14,11 @@ def print_stance_statistics(data: pd.DataFrame):
 def main():
     target = 'Climate Change is a Real Concern'
 
-    # Load training data
-    train_data_path = 'semeval2016-task6-trainingdata.txt'
-    train_data = load_sem_eval_data(train_data_path, target) 
+    train_data, test_data = load_sem_eval_data(target)  
+
     #print(train_data)
     #print_stance_statistics(train_data)
 
-    # Load test data
-    test_data_path = 'SemEval2016-Task6-subtaskA-testdata-gold.txt'
-    test_data = load_sem_eval_data(test_data_path, target)
     #print(test_data)
     #print_stance_statistics(test_data)
 
@@ -35,7 +29,7 @@ def main():
     X_test = test_data['Tweet']
     y_test = test_data['Stance']
 
-    # Create n_grams
+    # Vectorize data
     vectorizer = CountVectorizer(ngram_range=(4, 12)) # 4-grams to 12-grams seems to work best
     X_train = vectorizer.fit_transform(X_train)
     X_test = vectorizer.transform(X_test)
@@ -48,8 +42,9 @@ def main():
     # Make predictions on test data
     y_pred = classifier.predict(X_test)
 
-    # Calculate macro-average F1 score
-    mask = (y_test != 'NONE')
+    # Calculate macro-average F1 score as per SemEval-2016 Task 6 Task A specifications
+    # Should be a macro-average of the F1 scores of the Positive and Negative classes only
+    mask = (y_test != 'NONE') # Remove tweets that are not positive or negative
     y_test_filtered = y_test[mask]
     y_pred_filtered = y_pred[mask]
 
