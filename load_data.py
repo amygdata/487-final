b90905f869eaa1c3048eb95c5a1a8b10d3e1d865
amygdata <39467124+amygdata@users.noreply.github.com>
2023-11-20 20:26:47 -0500

    Preliminary Naive Bayes Implementation

---
+++ load_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import chardet 
+
+def detect_encoding(file_path: str):
+    """
+    Detects the encoding of a file
+    """
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+
+    return result['encoding']
+
+def load_sem_eval_data(txt_path: str, target: str):
+    """
+    Loads the SemEval 2016 dataset and extracts rows that contain the target
+
+    Args:
+        txt_path: Path to the SemEval 2016 dataset (.txt format)
+        target: The target to extract from the dataset
+
+    Returns:
+        Pandas dataframe containing the dataset
+    """
+    # Load dataset
+    data = pd.read_csv(txt_path, sep='\t', encoding=detect_encoding(txt_path))
+
+    # Remove targets we do not care about
+    data = data.loc[data['Target'] == target]
+
+    # Remove '#SemSt' from the 'Tweet' column
+    data['Tweet'] = data['Tweet'].str.replace('#SemST', '')
+
+    return data
---
+++ naive_bayes.py
@@ -0,0 +1,61 @@
+from load_data import load_sem_eval_data
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import f1_score
+
+def print_stance_statistics(data: pd.DataFrame):
+    """
+    Prints the number of tweets in each stance
+    """
+    stance_counts = data['Stance'].value_counts()
+    print(stance_counts)
+
+def main():
+    target = 'Climate Change is a Real Concern'
+
+    # Load training data
+    train_data_path = 'semeval2016-task6-trainingdata.txt'
+    train_data = load_sem_eval_data(train_data_path, target) 
+    #print(train_data)
+    #print_stance_statistics(train_data)
+
+
+    # Load test data
+    test_data_path = 'SemEval2016-Task6-subtaskA-testdata-gold.txt'
+    test_data = load_sem_eval_data(test_data_path, target)
+    #print(test_data)
+    #print_stance_statistics(test_data)
+
+
+    # Split data into X and y
+    X_train = train_data['Tweet']
+    y_train = train_data['Stance']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Stance']
+
+    # Convert text data into numerical data
+    vectorizer = CountVectorizer()
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
+
+    # Create and train Naive Bayes Classifier
+    classifier = MultinomialNB()
+    classifier.fit(X_train, y_train)
+
+
+    # Make predictions on test data
+    y_pred = classifier.predict(X_test)
+
+    # Calculate macro-average F1 score
+    mask = (y_test != 'NONE')
+    y_test_filtered = y_test[mask]
+    y_pred_filtered = y_pred[mask]
+
+    f1 = f1_score(y_test_filtered, y_pred_filtered, average='macro')
+    print(f1)
+
+
+if __name__ == '__main__':
+    main()
